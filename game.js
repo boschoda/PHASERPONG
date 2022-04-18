17f60ae552ef029b1ffef986373d9ea6278be2ea
boschoda <boschoda@mail.uc.edu>
2022-04-18 16:36:27 -0400

    working on js

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
 window.onload = function(){
-    var game = new Phaser.Game();
+    var game = new Phaser.Game(800,600,Phaser.AUTO,'',{preload : preload, create: create, 
+        update: update});
+    
+    function preload(){
+
+    }
+    
+    function create(){
+
+    }
+
+    function update(){
+        
+    }
 }
