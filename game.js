f7785de902c2060c2e2128d5a22d0d250ce8b32a
boschoda <boschoda@mail.uc.edu>
2022-04-19 18:47:15 -0400

    working on js

--- game.js
+++ game.js
@@ -12,64 +12,64 @@ var ball_launched;
 var ball_velocity;
 
 function preload(){
-game.load.image('paddle','assets/images/paddle.png');
-game.load.image('ball','assets/images/ball.png');
+    game.load.image('paddle','assets/images/paddle.png');
+    game.load.image('ball','assets/images/ball.png');
 }
 
 function create(){
-ball_launched = false;
-ball_velocity = 400;
+    game.add.image(32,256,'paddle');
+    
+    ball_launched = false;
+    ball_velocity = 400;
+    paddle1 = create_paddle(0,game.world.centerY);
+    paddle2 = create_paddle(game.world.width - 16,game.world.centerY); 
+    ball = create_ball(game.world.centerX,game.world.centerY);
 
-paddle1 = create_paddle(0,game.world.centerY);
-paddle2 = create_paddle(game.world.width - 16,game.world.centerY); 
-ball = create_ball(game.world.centerX,game.world.centerY);
-
-game.input.onDown.add(launch_ball, this);
+    game.input.onDown.add(launch_ball, this);
 }
 
 function update(){
-control_paddle(paddle1,game.input.y);
+    control_paddle(paddle1,game.input.y);
 }
 
 function create_paddle(x,y){
-paddle = game.add.sprite(x,y,'paddle');
-paddle.anchor.setTo(0.5,0.5);
-game.physics.arcade.enable(paddle);
-paddle.body.collideWorldBounds = true;
-
-return paddle;
+    paddle = game.add.sprite(x,y,'paddle');
+    paddle.anchor.setTo(0.5,0.5);
+    game.physics.arcade.enable(paddle);
+    paddle.body.collideWorldBounds = true;
+    
+    return paddle;
 }
 
 function control_paddle(paddle,y){
-paddle.y = y;
+    paddle.y = y;
 
-if(paddle.y < paddle.height / 2){
-paddle.y = paddle.height / 2;
-} else if (paddle.y > game.world.height - paddle.height / 2){
-paddle.y = game.world.height - paddle.height / 2;
+    if(paddle.y < paddle.height / 2){
+        paddle.y = paddle.height / 2;
+    } else if (paddle.y > game.world.height - paddle.height / 2){
+        paddle.y = game.world.height - paddle.height / 2;
 } 
-
 }
 
 function create_ball(x,y){
-ball = game.add.sprite(x,y,'ball');
-ball.anchor.setTo(0.5,0.5);
-game.physics.arcade.enable(ball);
-ball.body.collideWorldBounds = true;
-ball.body.bounce.setTo(1,1);
+    ball = game.add.sprite(x,y,'ball');
+    ball.anchor.setTo(0.5,0.5);
+    game.physics.arcade.enable(ball);
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.setTo(1,1);
 
-return ball;
+    return ball;
 }
 
 function launch_ball(){
-if(ball_launched){
-ball.x = game.world.centerX;
-ball.y = game.world.centerY;
-ball.body.velocity.setTo(0,0);
-ball_launched = false;
-}else {
-ball.body.velocity.x = -ball_velocity;
-ball.body.velocity.y = ball_velocity;
-ball_launched = true;
+    if(ball_launched){
+        ball.x = game.world.centerX;
+        ball.y = game.world.centerY;
+        ball.body.velocity.setTo(0,0);
+        ball_launched = false;
+    }else {
+        ball.body.velocity.x = -ball_velocity;
+        ball.body.velocity.y = ball_velocity;
+        ball_launched = true;
 }
 }
