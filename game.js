73962cc654d725f77522250f25d8efa3c52f74b2
boschoda <boschoda@mail.uc.edu>
2022-04-19 22:06:02 -0400

    game is functional, working on audio

--- game.js
+++ game.js
@@ -10,32 +10,61 @@ var paddle2;
 var ball;
 var ball_launched;
 var ball_velocity;
+var score1_text;
+var score2_text;
+var score1;
+var score2;
 
 function preload(){
     game.load.image('paddle','/assets/images/paddle.png');
     game.load.image('ball','/assets/images/ball.png');
+    game.load.audio('hit','/assets/hit.ogg');
 }
 
 function create(){
     ball_launched = false;
-    ball_velocity = 400;
+    ball_velocity = 500;
     paddle1 = create_paddle(0,game.world.centerY);
-    paddle2 = create_paddle(game.world.width - 16,game.world.centerY);
+    paddle2 = create_paddle(game.world.width - 8,game.world.centerY);
     ball = create_ball(game.world.centerX,game.world.centerY);
 
     game.input.onDown.add(launch_ball, this);
+
+    score1_text = game.add.text(128,128,'0',{
+      font: "64px Arial",
+      fill:"#ffffff",
+      align: "center"
+    });
+    score2_text = game.add.text(game.world.width - 128, 128 ,'0',{
+      font: "64px Arial",
+      fill:"#ffffff",
+      align: "center"
+    });
+
+    score1 = 0;
+    score2 = 0;
 }
 
 function update(){
+    score1_text.text = score1;
+    score2_text.text = score2;
+
     control_paddle(paddle1,game.input.y);
-    game.physics.arcade.collide(paddle1,ball);
-    game.physics.arcade.collide(paddle2,ball);
+    game.physics.arcade.collide(paddle1,ball,function(){
+      game.sound.play('hit');
+    });
+    game.physics.arcade.collide(paddle2,ball,function(){
+      game.sound.play('hit');
+    });
 
     if(ball.body.blocked.left){
-        console.log('Player 2 scores');
+        score2 += 1;
     } else if(ball.body.blocked.right){
-        console.log('Player 1 scores');
+        score1 += 1;
     }
+    paddle2.body.velocity.setTo(ball.body.velocity.y);
+    paddle2.body.velocity.x = 0;
+    paddle2.body.maxVelocity.y = 250;
 }
 
 function create_paddle(x,y){
@@ -44,6 +73,7 @@ function create_paddle(x,y){
     game.physics.arcade.enable(paddle);
     paddle.body.collideWorldBounds = true;
     paddle.body.immovable = true;
+    paddle.scale.setTo(0.5,0.5);
 
     return paddle;
 }
